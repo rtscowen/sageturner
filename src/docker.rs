8b9c68470ef4a588ef727109baa0db80f19fccc4
Robert Scowen <rtscowen@gmail.com>
2025-02-16 23:54:24 +0000

    Working dockerfile push to ECR

--- src/docker.rs
+++ src/docker.rs
@@ -1,14 +1,20 @@
 use std::{fs::File, io::Read};
 
-use bollard::{image::BuildImageOptions, Docker};
+use anyhow::{anyhow, Result, Context};
+use aws_sdk_ecr::{error::SdkError, operation::describe_repositories::DescribeRepositoriesError};
+use base64::prelude::*;
+use bollard::{auth::DockerCredentials, image::{BuildImageOptions, PushImageOptions, TagImageOptions}, secret::{BuildInfo, ImageId}, Docker};
 use tempfile::tempdir; 
 use tar::Builder;
 
 use futures_util::stream::StreamExt;
 
-pub async fn build_image(path: &str) {
-    let docker = Docker::connect_with_socket_defaults().unwrap(); 
+pub async fn get_client() -> Docker {
+    Docker::connect_with_socket_defaults().unwrap()
+}
 
+pub async fn build_image(path: &str, docker: &Docker, repo_name: &str) -> Result<()> {
+    println!("Building docker image at {path}, to repo {repo_name}");
     let temp_dir = tempdir().unwrap();
 
     let tar_path = temp_dir.path().join("archive.tar");
@@ -17,23 +23,99 @@ pub async fn build_image(path: &str) {
     builder.append_dir_all("", path).unwrap();
     builder.finish().unwrap();
 
-    let options = BuildImageOptions{
-        dockerfile: "Dockerfile",
-        t: "image", 
-        rm: true,
-        ..Default::default()
-    };
-
+    
     let mut archive = File::open(tar_path).unwrap();
     let mut contents = Vec::new();
     archive.read_to_end(&mut contents).unwrap();
-
+    
+    let options = BuildImageOptions{
+        dockerfile: "Dockerfile",
+        t: repo_name, 
+        rm: true,
+        ..Default::default()
+    };
     let mut build = docker.build_image(options, None, Some(contents.into()));
 
+    let mut image_id: String = "".to_string();
     while let Some(msg) = build.next().await {
-        let status = msg.unwrap().status.unwrap();
-        println!("Building docker image.... {status}");
+        let build_output = msg?;
+        print!("{}", build_output.stream.unwrap_or_default());
+        if let BuildInfo { aux: Some(ImageId { id: Some(id) }), .. } = build_output {
+            image_id = id;
+        }
     }
 
+    if image_id.is_empty() {
+        Err(anyhow!("No image tag"))
+    } else {
+        Ok(())
+    }
 
+}
+
+pub async fn push_image(docker: &Docker, ecr_client: &aws_sdk_ecr::Client, repo_name: &str) -> Result<()> {
+    println!("Pushing image to repo: {repo_name}");
+    let repo_check = ecr_client.describe_repositories().repository_names(repo_name).send().await;
+    let uri;
+    match repo_check {
+        Ok(desc) => {
+            uri = desc.repositories()[0].repository_uri.clone().ok_or_else(|| anyhow!("Error reading repo URI"))?;
+        },
+        Err(err) => {
+            match err.into_service_error() {
+                DescribeRepositoriesError::RepositoryNotFoundException(_) => {
+                    let new_repo = ecr_client.create_repository()
+                        .repository_name(repo_name)
+                        .send()
+                        .await?;
+
+                    let new_repo_info = new_repo.repository().ok_or_else(|| anyhow!("Error reading new repo info"))?;
+                    uri = new_repo_info.repository_uri.clone().ok_or_else(|| anyhow!("Error reading new repo URI"))?
+                },
+                err @ _ => return Err(err.into()),
+            };
+        },
+    };
+        
+    docker.tag_image(repo_name, Some(TagImageOptions{
+        tag: "latest",
+        repo: &uri
+    })).await?;
+
+    let push_options = Some(PushImageOptions::<String> {
+        tag: "latest".to_string()
+    });
+    let credentials = get_docker_credentials(ecr_client).await?;
+    let mut push_stream = docker.push_image(&uri, push_options, Some(credentials));
+
+    while let Some(stream) = push_stream.next().await {
+        let info = stream?;
+        let progess = info.progress;
+        println!("{:?}", progess)
+    }
+    Ok(())
+}
+
+async fn get_docker_credentials(ecr_client: &aws_sdk_ecr::Client) -> Result<DockerCredentials> {
+    println!("Getting Docker Credentials ");
+    // assume default registry
+    let ecr_auth = ecr_client.get_authorization_token()
+        .send()
+        .await?;
+
+    let token = ecr_auth.authorization_data()[0].authorization_token().ok_or_else(|| anyhow!("Couldn't read auth token"))?;
+    let decoded_token = BASE64_STANDARD.decode(token)?;
+
+    let parts: Vec<_> = decoded_token.split(|c| *c == b':').collect(); // split at : for user:password
+    let username = String::from_utf8(parts[0].to_vec()).unwrap();
+    let password = String::from_utf8(parts[1].to_vec()).unwrap();
+
+    let endpoint = ecr_auth.authorization_data()[0].proxy_endpoint().ok_or_else(|| anyhow!("Couldn't read proxy endpoint"))?;
+
+    Ok(DockerCredentials{
+        username: Some(username),
+        password: Some(password),
+        serveraddress: Some(endpoint.to_string()),
+        ..Default::default()
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use argh::FromArgs;
 
+use anyhow::Result;
+use base64::prelude::*;
+use bollard::{auth::DockerCredentials, image::{PushImageOptions, TagImageOptions}, Docker};
+use futures_util::StreamExt;
+
 mod docker;
 
 #[derive(Debug, FromArgs, PartialEq)]
@@ -27,6 +32,9 @@ struct Deploy {
 
     #[argh(option, short='e', description="endpoint type")]
     endpoint_type: String,
+
+    #[argh(option, short='r', description="repo name")]
+    repo_name: String,
 }
 
 
@@ -35,41 +43,44 @@ struct Deploy {
 struct Setup {}
 
 #[::tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cmd : SimpleSageCLI = argh::from_env();
 
     let config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
-    let client = aws_sdk_sagemaker::Client::new(&config);
+    let sage_client = aws_sdk_sagemaker::Client::new(&config);
+    let ecr_client = aws_sdk_ecr::Client::new(&config);
 
+    let docker = docker::get_client().await; 
 
     match cmd.nested {
-        SimpleSageSubCommands::Deploy(deploy) => process_deploy(&client, &deploy).await,
-        SimpleSageSubCommands::Setup(setup) => process_setup(&client, &setup).await,
+        SimpleSageSubCommands::Deploy(deploy) => process_deploy(&ecr_client, &sage_client, &docker, &deploy).await.unwrap(),
+        SimpleSageSubCommands::Setup(setup) => process_setup(&ecr_client, &sage_client, &docker, &setup).await.unwrap(),
     }
+
+    Ok(())
 }
 
-async fn process_deploy(client: &aws_sdk_sagemaker::Client, deploy_params: &Deploy) {
-
-    // parse the wire YAML 
-    // build the image programatically 
-    // push the image to ECR 
-    // 
-
+async fn process_deploy(ecr_client: &aws_sdk_ecr::Client, sage_client: &aws_sdk_sagemaker::Client, docker_client: &Docker, deploy_params: &Deploy) -> Result<()> {
+    println!("Deploying model located at: {}", &deploy_params.wire_file);
+    docker::build_image(&deploy_params.dockerfile, docker_client, &deploy_params.repo_name).await?; 
+    docker::push_image(docker_client, ecr_client, &deploy_params.repo_name).await?;
+    
     // // Create a model, specifying this container
     // client.create_model().containers(input)
-
+    
     // // Reference the container as a production variant in the endpoint config (serverless)
     // let production_variants: Vec<String> = vec![]
-
-
+    
+    
     // client.create_endpoint_config()
     //     .set_endpoint_config_name(Some("endpoint_1".to_string()))
     //     .set_production_variants(input);
     
     // // Create the endpoint 
     // client.create_endpoint();
+    Ok(())
 }
 
-async fn process_setup(client: &aws_sdk_sagemaker::Client, setup_params: &Setup) {
+async fn process_setup(ecr_client: &aws_sdk_ecr::Client, sage_client: &aws_sdk_sagemaker::Client, docker_client: &Docker, deploy_params: &Setup) -> Result<()> {
     todo!()
 }
