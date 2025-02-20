0dd204ae697c82c4ccf024f5403e7cba69b9a1dd
Robert Scowen <rtscowen@gmail.com>
2025-02-20 19:41:05 +0000

    Refactored config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use argh::FromArgs;
+use std::str::FromStr;
 
+use argh::FromArgs;
 use anyhow::{anyhow, Result};
 use bollard::Docker;
 
@@ -8,31 +9,88 @@ mod aws;
 mod model_config;
 
 #[derive(Debug, FromArgs, PartialEq)]
-#[argh(description="SimpleSage deploys your models to AWS SageMaker in one command")]
-struct SimpleSageCLI {
+#[argh(description="Sageturner deploys your models to Amazon SageMaker in one command.")]
+struct SageturnerCLI {
     #[argh(subcommand)]
-    nested: SimpleSageSubCommands
+    nested: SageturnerSubCommands
 }
 
 #[derive(Debug, FromArgs, PartialEq)]
 #[argh(subcommand)]
-enum SimpleSageSubCommands {
+enum SageturnerSubCommands {
     Deploy(Deploy),
 }
 
 #[derive(Debug, FromArgs, PartialEq)]
-#[argh(subcommand, name="deploy", description="Deploy serialised models directly to Sagemaker endpoint")]
+#[argh(subcommand, name="deploy", description="Deploy models to Sagemaker endpoints")]
 struct Deploy {
-    #[argh(option, short='e', description="endpoint type (only serverless supported)", default="String::from(\"serverless\")")]
-    endpoint_type: String, 
+    #[argh(option, short='e', description="the type of endpoint for deployment (serverless or server)")]
+    endpoint_type: EndpointType, 
 
-    #[argh(option, short='m', description="path to model config YAML")]
+    #[argh(option, short='m', description="sageturner deployment mode: docker (supply your own dockerfile) or smart (sageturner builds one for you)")]
+    mode: DeploymentMode,
+
+    #[argh(option, short='c', description="path to sageturner.yaml")]
     model_config: String
+}
+
+#[derive(Debug, PartialEq)]
+enum EndpointType {
+    Serverless,
+    Server
+}
+
+impl FromStr for EndpointType {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "serverless" => Ok(EndpointType::Serverless),
+            "server" => Ok(EndpointType::Server),
+            _ => Err(anyhow!("Invalid endpoint type. serverless or server only, not: {}", s))
+        }
+    }
+}
+
+impl std::fmt::Display for EndpointType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EndpointType::Serverless => write!(f, "serverless"),
+            EndpointType::Server => write!(f, "server"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum DeploymentMode {
+    Docker, 
+    Smart
+}
+
+impl FromStr for DeploymentMode {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "smart" => Ok(DeploymentMode::Smart),
+            "docker" => Ok(DeploymentMode::Docker),
+            _ => Err(anyhow!("Invalid deployment type. docker or smart, not: {}", s))
+        }
+    }
+}
+
+impl std::fmt::Display for DeploymentMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DeploymentMode::Docker => write!(f, "docker"),
+            DeploymentMode::Smart => write!(f, "smart"),
+        }
+    }
 }
 
 #[::tokio::main]
 async fn main() -> Result<()> {
-    let cmd : SimpleSageCLI = argh::from_env();
+    let cmd : SageturnerCLI = argh::from_env();
 
     let config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
     let sage_client = aws_sdk_sagemaker::Client::new(&config);
@@ -40,19 +98,28 @@ async fn main() -> Result<()> {
     let iam_client = aws_sdk_iam::Client::new(&config);
     let s3_client = aws_sdk_s3::Client::new(&config);
 
-    let docker = docker::get_client().await; 
+    let docker = docker::get_client().await;
 
     match cmd.nested {
-        SimpleSageSubCommands::Deploy(deploy) => process_deploy(&ecr_client, &sage_client, &docker, &iam_client, &s3_client, &deploy).await.unwrap(),
+        SageturnerSubCommands::Deploy(deploy) => process_deploy(&ecr_client, &sage_client, &docker, &iam_client, &s3_client, &deploy).await?,
     }
 
     Ok(())
 }
 
-async fn process_deploy(ecr_client: &aws_sdk_ecr::Client, sage_client: &aws_sdk_sagemaker::Client, docker_client: &Docker, iam_client: &aws_sdk_iam::Client, s3_client: &aws_sdk_s3::Client, deploy_params: &Deploy) -> Result<()> {
-    println!("Deploying model with config {} to {} endpoint type", &deploy_params.model_config, &deploy_params.endpoint_type);
+async fn process_deploy(ecr_client: &aws_sdk_ecr::Client, 
+                        sage_client: &aws_sdk_sagemaker::Client, 
+                        docker_client: &Docker, 
+                        iam_client: &aws_sdk_iam::Client, 
+                        s3_client: &aws_sdk_s3::Client, 
+                        deploy_params: &Deploy) 
+                        -> Result<()> {
+    println!("Deploying model with config at {} to {} endpoint, {} deployment mode", &deploy_params.model_config, &deploy_params.endpoint_type, &deploy_params.mode);
 
     let model_config = model_config::parse_config(deploy_params.model_config.into())?;
+    model_config::validate_config(&model_config, &deploy_params.endpoint_type, &deploy_params.mode)?;
+
+    // container_mode 
 
     // EZ Mode vs user supplied docker file
     match model_config.code {
@@ -94,16 +161,16 @@ async fn process_deploy(ecr_client: &aws_sdk_ecr::Client, sage_client: &aws_sdk_
             // override bucket and use default for role
             let bucket_name = overrides.bucket.ok_or_else(|| anyhow!("Something went wrong with our config parsing. Raise an issue"))?;
             aws::create_sagemaker_bucket(&bucket_name, s3_client).await?;
-            exec_role_arn = aws::create_sagemaker_role("simplesage-role", iam_client).await?;
+            exec_role_arn = aws::create_sagemaker_role("Sageturner-role", iam_client).await?;
         } else if should_override_role {
             // override role and use default for bucket 
             let role_name = overrides.role.ok_or_else(|| anyhow!("Something went wrong with our config parsing. Raise an issue"))?;
-            aws::create_sagemaker_bucket("simplesage-sagemaker", s3_client).await?;
+            aws::create_sagemaker_bucket("Sageturner-sagemaker", s3_client).await?;
             exec_role_arn = aws::create_sagemaker_role(&role_name, iam_client).await?;
         }
     } else {
-        exec_role_arn = aws::create_sagemaker_role("simplesage-role", iam_client).await?;
-        aws::create_sagemaker_bucket("simplesage-sagemaker", s3_client).await?; // needs to have sagemaker in it for policy
+        exec_role_arn = aws::create_sagemaker_role("Sageturner-role", iam_client).await?;
+        aws::create_sagemaker_bucket("Sageturner-sagemaker", s3_client).await?; // needs to have sagemaker in it for policy
     }
 
     aws::create_sagemaker_model(&model_config.name, &exec_role_arn, &uri, sage_client).await?;
--- src/model_config.rs
+++ src/model_config.rs
@@ -3,37 +3,62 @@ use std::{fs::File, io::Read, path::PathBuf};
 use anyhow::{Result, anyhow};
 use serde::Deserialize;
 
+use crate::{DeploymentMode, EndpointType};
+
 #[derive(Debug, Deserialize)]
 pub struct ModelConfig {
     // The name of the model 
     pub name: String,
     // The model artefact. If provided, we upload this to (compressing if uncompressed) to S3 
-    // and pass the S3 path as ModelDataURI to the endpoint. Sagemaker then makes this available
-    // to the container at /opt/ml/model
+    // and pass the S3 path as ModelDataURI to the endpoint. SageMaker then makes this available
+    // to the container at /opt/ml/model, boosting load times
     pub artefact: Option<String>,
-    // A Python file containing a load() and predict() function. We call these functions from a
-    // template serve.py file. This option is for Smart (Dockerless) deploys only, and is mutually
-    // exclusive with docker_dir
-    pub code: Option<String>,
-    // Optional additional system packages to install to container when running a smart deploy
-    pub system_packages: Option<String>,
-    // Optional additional python packages to install to container when running a smart deploy
-    // (things that load() and predict() depend on). Don't need to provide FastAPI, that's in the 
-    // serve.py template
-    pub python_packages: Option<String>,
-    // Whether to install CUDA for a smart deploy. Note that we don't allow this if deploy mode is serverless,
-    // as there's no point since Serverless endpoints can't use GPUs
-    pub enable_cuda: Option<bool>,
-    // The python version for a smart deploy. 
-    pub python_version: Option<String>,
-    // If bringing your own Dockerfile, provide the directory where we can find the Dockerfile to build.
-    // We bundle everything in that directory to a TAR as part of the build process
-    pub docker_dir: Option<String>,
+    // Deployment configuration(s)
+    pub deployment: Deployment,
     // Specify compute characterstics 
     pub compute: Compute,
-    // Override the default role and bucket names created by SimpleSage as part of the deploy process. 
+    // Override the default role and bucket names created by Sageturner as part of the deploy process. 
     // Creates the role/bucket if they don't exist, but won't error if they do
     pub sagemaker_overrides: Option<Overrides>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Deployment {
+    // Configuration for smart mode deploy 
+    pub smart_deploy: Option<SmartDeployConfig>,
+    // Configuration for a docker mode deploy
+    pub docker_deploy: Option<DockerDeployConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+struct SmartDeployConfig {
+    // A path to a Python script containing a load() and predict() function. We call these functions from a
+    // template serve.py file. This option is for smart mode deploys only
+    pub code: String,
+    // Optional additional system packages to install to container when running a smart deploy
+    pub system_packages: Option<Vec<String>>,
+    // Optional python packages to install to container when running a smart deploy
+    // (things that load() and predict() depend on, for instance). Don't need to provide FastAPI, that's in the 
+    // serve.py template
+    pub python_packages: Option<Vec<String>>,
+    // Whether to install CUDA for a smart deploy. Note that we don't allow this if deploy mode is serverless,
+    // as there's no point since Serverless endpoints can't use GPUs
+    pub install_cuda: bool,
+    // The python version for a smart deploy. 
+    // defaults to 3.12
+    #[serde(default = "default_python")] 
+    pub python_version: String,
+}
+
+fn default_python() -> String {
+    "3.12".to_string()
+}
+
+#[derive(Debug, Deserialize)]
+struct DockerDeployConfig {
+    // If bringing your own Dockerfile, provide the directory where we can find the Dockerfile and artefacts to build.
+    // We bundle everything in that directory to a TAR as part of the build process, so paths referenced in Docker COPY commands needs to work in that directory 
+    pub docker_dir: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -71,58 +96,69 @@ pub fn parse_config(path: PathBuf) -> Result<ModelConfig> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let config: ModelConfig = serde_yaml::from_str(&contents)?;
-
-    validate_config(&config)?;
-
-    Ok(config)
+    match serde_yaml::from_str::<ModelConfig>(&contents) {
+        Ok(mc) => Ok(mc),
+        Err(err) => {
+            match err.location() {
+                Some(l) => {
+                    let location = format!("line {} column {}", l.line(), l.column());
+                    Err(anyhow!("YAML parsing error at {}: {}", location, err))
+                },
+                None => Err(err.into()),
+            }
+        },
+    }
 }
 
-fn validate_config(mc: &ModelConfig, endpoint_type: &str) -> Result<()> {
+pub fn validate_config(mc: &ModelConfig, endpoint_type: &EndpointType, deploy_mode: &DeploymentMode) -> Result<()> {
     println!("Validating config file");
-    // predict-code and docker-dir/python/system packages cannot both be present 
-    // validate that the user is either bringing own dockerfile, or using SimpleSage dockerfree deploys
-    println!("Validating own dockerfile vs SimpleSage dockerfree deploy")
-    if mc.docker_dir.is_some() && (mc.code.is_some() || mc.system_packages.is_some() || mc.python_packages.is_some() || mc.enable_cuda.is_some() || mc.python_version.is_some()) {
-        return Err(anyhow!("code/system_packages/python_packages/enabled_cuda are for smart (dockerfile free) deploy mode, 
-        and mutually exclusive with docker_dir (which is for bringing your own Dockerfile). 
-        If you're bringing your own Dockerfile, 
-        you're responsible for copying the server code, installing packages, and CUDA"));
+    if mc.name.is_empty() {
+        return Err(anyhow!("Invalid sageturner config: model name can't be an empty string"));
     }
 
-    let smart_deploy = mc.docker_dir.is_none();
-    if smart_deploy {
-        // validate we have minimal fields for a smart deploy
-        if mc.code.is_none() {
-            return Err(anyhow!("ERROR: You're attempting a smart deploy, but you've provided no code file that tells us how to load model and run inference"));
-        }
+    if mc.artefact.as_ref().is_some_and(|a| a.is_empty()) {
+        return Err(anyhow!("Invalid sageturner config: artefact can't be an empty string"));
+    }
 
-        // warn users about blank fields. these aren't required and we do sensible things as default, 
-        // but we should let users know they're missing and what the consequences are
-        if mc.enable_cuda.is_none() {
-            println!("WARNING: You didn't set enable_cuda. Defaulting to false for dockerfree deploy");
-        }
+    // Validate minimal config present for each deploy mode 
+    match deploy_mode {
+        DeploymentMode::Docker => {
+            if mc.deployment.docker_deploy.is_none() {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy in docker mode, without field docker_deploy"));
+            }
+            if mc.deployment.docker_deploy.as_ref().is_some_and(|d| d.docker_dir.is_empty()) {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy in docker mode, but your docker_dir is an empty string"));
+            }
+        },
+        DeploymentMode::Smart => {
+            if mc.deployment.smart_deploy.is_none() {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy in smart mode, without field smart_deploy"));
+            }
+            if mc.deployment.smart_deploy.as_ref().is_some_and(|s| s.code.is_empty()) {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy in smart mode, but your code field is an empty string. needs to be path to code with load() and predict()"));
+            }
+        },
+    }
 
-        if mc.python_packages.is_none() {
-            println!("WARNING: You didn't set any additional python packages for dockerfree deploy. The generated container won't have any PIP packages except FastAPI");
-        }
+    // Validate minimal config present for each endpoint type 
+    match endpoint_type {
+        EndpointType::Serverless => {
+            if mc.compute.serverless.is_none() {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy to a serverless endpoint, without compute->serverless field"));
+            }
+        },
+        EndpointType::Server => {
+            if mc.compute.server_compute.is_none() {
+                return Err(anyhow!("Invalid sageturner config: you're trying to deploy to a server endpoint, without compute->server field"));
+            }
+        },
+    }
 
-        if mc.system_packages.is_none() {
-            println!("WARNING: You didn't set any additional system packages for dockerfree deploy. The generated container won't apt-get install any additional packages");
-        }
-
-        if mc.python_version.is_none() {
-            println!("WARNING: You didn't set python version for a dockerfree deploy. We'll default to 3.12");
-        }
-
-        // Disallow cuda-enabled if the endpoint type is serverless. The user might expect a GPU 
-        if mc.enable_cuda.is_some_and(|x| x) && endpoint_type == "serverless" {
-            return(Err(anyhow!("ERROR: You're attempting a dockerfree serverless deployment, but you've got CUDA enabled. Sagemaker doesn't
-             support GPUs for serverless inference. You need to bring your own Dockerfile, and install CUDA toolkit yourself (thoughts and prayers)")));
-        }
-    } else {
-        // what are the conditions for a bring your own dockerfile deploy
-
+    // Special case: GPUs not supported on serverless
+    if *endpoint_type == EndpointType::Serverless && 
+    *deploy_mode == DeploymentMode::Smart && 
+    mc.deployment.smart_deploy.as_ref().is_some_and(|s| s.install_cuda) {
+        return Err(anyhow!("Invalid sageturner config: you're trying to do a smart deploy, to a serverless endpoint, with install_cuda as true. Serverless endpoints don't support GPU, so no point installing CUDA when sageturner generates your container"));
     }
 
     Ok(())
